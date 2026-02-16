77523c3e02d9d37c82ab6e00f8edec83a46e052d
Daney Koster <daneykoster@gmail.com>
2026-02-16 07:44:08 +0100

    refactor: Change Bricks element registration hook from `bricks/register_elements` to `init` with priority 11.

--- includes/bricks/integration.php
+++ includes/bricks/integration.php
@@ -4,7 +4,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 /**
  * Register Bricks Elements
  */
-add_action( 'bricks/register_elements', function() {
+add_action( 'init', function() {
     $element_files = [
         __DIR__ . '/elements/product-video-gallery.php',
     ];
@@ -18,4 +18,4 @@ add_action( 'bricks/register_elements', function() {
     if ( class_exists( 'Bricks_Product_Video_Gallery' ) ) {
         \Bricks\Elements::register_element( new \Bricks_Product_Video_Gallery() );
     }
-} );
+}, 11 );
